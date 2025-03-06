6f01a7447a8e61c39016c5138c124828e8de85e7
sganeshwani <ganeshwani2003@gmail.com>
2025-03-06 13:49:05 +0530

    [Sahil Ganeshwani] Add to display Hello from BridgeLabz with a user inputted name

--- HelloWorld/src/app/app.component.ts
+++ HelloWorld/src/app/app.component.ts
@@ -1,22 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms'; // Import FormsModule for Two-Way Binding
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule], // Add FormsModule here
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'HelloWorld';
-  logoUrl = '/assets/BL_logo_square_jpg.jpg'; // BridgeLabz Logo URL
+  title = 'Hello from BridgeLabz';
+  logoUrl = 'assets/BL_logo_square_jpg.jpg';
+  userName: string = ''; // Stores the user input
 
-  ngOnInit(): void {
-    this.title = 'Hello from BridgeLabz';
-  }
+  ngOnInit(): void {}
 
   openBridgeLabz(): void {
-    window.open('https://www.bridgelabz.com', '_blank'); // Opens site in a new tab
+    window.open('https://www.bridgelabz.com', '_blank');
   }
 }
